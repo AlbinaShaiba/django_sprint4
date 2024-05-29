0ecd57c65712772e63d26526a57c673ae95f93c5
Albina Shaibakova <albinashaibakova@gmail.com>
2024-05-29 19:50:19 +0500

    pages app update

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,25 +2,31 @@ from django.views.generic import TemplateView
 
 from django.shortcuts import render
 
+
 class AboutPageView(TemplateView):
-    """ Returns about project page"""
+    """Returns about project page"""
+
     template_name = 'pages/about.html'
 
 class RulesPageView(TemplateView):
-    """ Returns project rules pages"""
+    """Returns project rules pages"""
+
     template_name = 'pages/rules.html'
 
 
 def page_not_found(request, exception=None):
-    """ Page not found"""
+    """Page not found"""
+
     return render(request, 'pages/404.html', status=404)
 
 
 def csrf_failure(request, reason=''):
-    """ CSRF Failure"""
+    """CSRF Failure"""
+
     return render(request, 'pages/403csrf.html', status=403)
 
 
 def server_error(request):
-    """ Server Error"""
+    """Server Error"""
+
     return render(request, 'pages/500.html', status=500)
